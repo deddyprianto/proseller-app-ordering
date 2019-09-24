ab829a49534415f165a5cb890e216b49aef3f68f
Chaerus Sulton <chaerussulton@gmail.com>
2019-09-24 14:21:17 +0700

    update config

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -1,11 +1,11 @@
 const awsConfig = {
   // Dev
-  awsUserPoolId: 'ap-southeast-1_06bLrZ4t8',
-  awsUserPoolWebClientId: 'noba3tnv3li8m1um0bvb21d8g',
+  // awsUserPoolId: 'ap-southeast-1_06bLrZ4t8',
+  // awsUserPoolWebClientId: 'noba3tnv3li8m1um0bvb21d8g',
 
   // Demo
-  // awsUserPoolId: 'ap-southeast-1_w525Hvp6g',
-  // awsUserPoolWebClientId: '2bt452vj3impp8iaq6cgdooq13',
+  awsUserPoolId: 'ap-southeast-1_w525Hvp6g',
+  awsUserPoolWebClientId: '2bt452vj3impp8iaq6cgdooq13',
 
   identityPoolId: 'ap-southeast-1:346468483688:userpool/ap-southeast-1_06bLrZ4t8',
   awsClientId: '7assk8plr257el191tk9rsouah',
