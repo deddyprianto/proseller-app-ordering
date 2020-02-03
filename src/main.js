cf79c094102d0f28e6ee8a05540bab676503243d
Muhammad Iqbal Dwi Putra <iqbal.dp@yahoo.com>
2020-02-03 15:11:02 +0700

    add push notif

--- src/main.js
+++ src/main.js
@@ -30,10 +30,18 @@ import Loader from './components/loader';
 import colorConfig from './config/colorConfig';
 import appConfig from './config/appConfig';
 import awsConfig from './config/awsConfig';
+import OneSignal from 'react-native-onesignal';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+    OneSignal.init('99fc4d7d-f836-425e-801c-1da1a06a7793', {
+      kOSSettingsKeyAutoPrompt: true,
+    }); // set kOSSettingsKeyAutoPrompt to false prompting manually on iOS
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
     this.state = {
       isLoading: true,
       // geolocation: false,
@@ -87,6 +95,27 @@ class Main extends Component {
       }, 1000),
     );
   };
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
 
   render() {
     const {
