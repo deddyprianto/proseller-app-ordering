d4bfe1e20d56d74b86d0a5f82b8b52a09836221c
Iqbal DP <iqbaldp@yahoo.co.id>
2020-07-22 23:02:01 -0700

    build ios

--- ios/QijiTests/QijiTests.m
+++ ios/QijiTests/QijiTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface QijiTests : XCTestCase
+@interface UstarsTests : XCTestCase
 
 @end
 
-@implementation QijiTests
+@implementation UstarsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
