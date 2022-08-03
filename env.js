e19547cb1f395606a814ff7845431b598209f67a
martinwijaya97 <martinwijaya97@gmail.com>
2022-08-03 13:07:06 +0700

    acemart

--- env.js
+++ env.js
@@ -1,12 +1,12 @@
 const ENV = {
-  APP: 'Fun Toast',
+  APP: 'AceMart',
   IS_DEVELOP: true,
 
-  API_HOST: 'https://api-funtoast.proseller-demo.com/',
+  API_HOST: 'https://acemart-demo.proseller.io/',
 
-  base_url_payment: 'https://payment.proseller-demo.com/api/',
+  base_url_payment: 'https://payment-demo.proseller.io/api/',
 
-  ONESIGNAL_ID: 'ecc78781-3da8-4d14-9df9-f6930d402523',
+  ONESIGNAL_ID: '33917c22-2105-4ba6-aba5-c4a3b996302a',
 
   REGION: 'ap-southeast-1',
 
@@ -16,35 +16,35 @@ const ENV = {
 
   COUNTRY: 'Singapore',
 
-  COMPANY_NAME: 'AutoMaxima',
+  COMPANY_NAME: 'Ace Mart',
 
-  COMPANY_POLICY_URL: 'https://funtoast.proseller-demo.com',
+  COMPANY_POLICY_URL: 'https://acemart-demo.proseller.io/',
 
   CURRENCY: 'SGD',
 
   LOCALES: 'en-US',
 
-  SENTRY_DSN:
-    'https://57b71e8fbd404c2bb5921afbb00b6c5c@o354451.ingest.sentry.io/2653850',
+  SENTRY_DSN: 'https://9ed8cf3be2e24a3e884a0b15f5f054d0@sentry.io/2653559',
 
   PRIVATE_KEY_RSA:
     'LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQogICAgICAgICAgICAgICAgICBNSUlDWEFJQkFBS0JnUUN0blg5Q3FSWlE0SndWRmxmdzd3TW9qdmxhVGt6clpZQ2d5alQ2L1prdUZDWWhkMytTCiAgICAgICAgICAgICAgICAgIEJUUGo0LzNMSkdwK0lnbnRLNENvVjVpQXFvdm9YQVBkM2Fkb0UrTUdsKzRtM2diK0xjVnpWOHIvbmNqdzIxaGgKICAgICAgICAgICAgICAgICAgRVIxY3JvaTljK3dORmhkYVRlemJTakZmNkdjVnNuNUFQMHNtcnA5SVFTeFNrWmFnbTRzWGtOSi9GUUlEQVFBQgogICAgICAgICAgICAgICAgICBBbjg2V1lqVVRjTThaS3l5UVRpWFMxRnBmQUk5OWZGSk9aUCtKWERsbVZ5YmJYUklXS2YybDFhRkRvcGNDVjNPCiAgICAgICAgICAgICAgICAgIHJ3Y01GMW1WY0dIOHBuMkF4bDZ1dktSMWlCdml2NTVqRE5lTnFGYm9hODhaOTk2aERxdm1iTTVwV0FJbk4rNWwKICAgICAgICAgICAgICAgICAgQTlodEhLRTQrTDIvSWhnaGZWa0ticEpYMlN0dzRpdmZ1ODRVcXF6WGlkamhBa0VBNVg1NldqbHhic25IV2t6VgogICAgICAgICAgICAgICAgICBvZFNxU0JEalBtUzVxZFVRTC9pWnJyWVQwZnJKazIyR1ZIaFdjSk9kTjA0UFRKZ3ZncFUyRXdXOHBITnR2clIwCiAgICAgICAgICAgICAgICAgIFlKcU5ZUUpCQU1HcTFSZVptdzNic29wWVc0NHF6aHZuRG1yc3A4Z21yNUVCaHJzMkcyaHU0RlJpaW1rZ2FJdUYKICAgICAgICAgICAgICAgICAgbzNQdVNrU0tOdDh3ZWowVnVxaEo3QXYxQi9OVGVqVUNRUUNnUjB3V2RXWDJsVEFJcmY3SjdtZ2F1c2lxeTlncgogICAgICAgICAgICAgICAgICBkNlc5aXlkeU1MSDVCZWtBY1E3UE1kaFUrWk5raTd6OXBwQVA2RHdDM3cvWDN0SDN6Ym8xdWRzaEFrRUFpejJXCiAgICAgICAgICAgICAgICAgIDdkdmJsakFpQXNPYU4vSGd0RUVBcGJHMncvbkVlcjRkTDFhc2gvNUh2WVFIdmFPMlpySFRuaE55UkNzWFhvcU4KICAgICAgICAgICAgICAgICAgS0lRSjZ0c1RJbG11dUt5SkRRSkJBTDRnaGVDTm53dlpUT0xtZlJtT01wSCszd2NPWWxSYlpQazlCZFpteHBxbgogICAgICAgICAgICAgICAgICA3MHNsbkcydDBBVWVlR1hNZFl3S3hZSTh2WXVsRWRrR2xsbS9FQURRaHlBPQogICAgICAgICAgICAgICAgICAtLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQ==',
 
-  appUrl: 'funtoast-crm://app',
+  appUrl: 'acemart-demo-crm://app',
 
-  appID: 'com.funtoast.app',
+  appID: 'com.acemart.demo',
 
-  retailerInfo: 'FunToast',
+  retailerInfo: 'Ace Dynamic UAT',
 
-  netsclick_terminal_id: '0',
+  netsclick_terminal_id: '36192101',
 
-  netsclick_retailer_id: '0',
+  netsclick_retailer_id: '11136192100',
 
-  netsclick_merchantHost: '/',
+  netsclick_merchantHost:
+    'https://im6ka3ygs4.execute-api.ap-southeast-1.amazonaws.com/uat',
 
-  netsclick_api_key: 'f',
+  netsclick_api_key: '4ec12e73-8afd-47b2-894a-71a2303c7514',
 
-  netsclick_secret_key: 'f',
+  netsclick_secret_key: '2f27ba6d-b7c8-4de3-8bfd-01480b7c6822',
 };
 
 export default ENV;
