4c3500814dfbf892979e800224b69b6f2d67ddfa
martinwijaya97 <martinwijaya97@gmail.com>
2022-09-09 13:35:58 +0700

    env live

--- env.js
+++ env.js
@@ -2,8 +2,8 @@ const ENV = {
   APP: 'AceMart',
   IS_DEVELOP: true,
 
-  API_HOST: 'https://acemart-demo.proseller.io/',
-  // API_HOST: 'https://api-acemart.proseller.io/',
+  // API_HOST: 'https://acemart-demo.proseller.io/',
+  API_HOST: 'https://api-acemart.proseller.io/',
 
   base_url_payment: 'https://payment-demo.proseller.io/api/',
 
