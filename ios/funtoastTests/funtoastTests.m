dc8023c4a4fa614f22f59a4724fd531ce06f4617
martinwijaya97 <martinwijaya97@gmail.com>
2022-09-14 14:00:27 +0700

    checkpoint

--- ios/funtoastTests/funtoastTests.m
+++ ios/funtoastTests/funtoastTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface edgeworksTests : XCTestCase
+@interface funtoastTests : XCTestCase
 
 @end
 
-@implementation edgeworksTests
+@implementation funtoastTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
