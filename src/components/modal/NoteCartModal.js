ffd89885a689045595020da0c14945fad7889243
martinwijaya97 <martinwijaya97@gmail.com>
2022-11-09 17:21:33 +0700

    fix

--- src/components/modal/NoteCartModal.js
+++ src/components/modal/NoteCartModal.js
@@ -96,14 +96,6 @@ const useStyles = () => {
       borderRadius: 8,
       backgroundColor: theme.colors.buttonActive,
     },
-    touchableSubmitDisabled: {
-      flex: 1,
-      paddingVertical: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 8,
-      backgroundColor: theme.colors.buttonDisabled,
-    },
     touchableCancel: {
       flex: 1,
       marginRight: 16,
@@ -165,37 +157,21 @@ const NoteCartModal = ({open, value, handleClose, handleSubmit}) => {
   const renderBody = () => {
     return <View style={styles.body}>{renderTextInput()}</View>;
   };
-  const renderButtonCancel = () => {
-    return (
-      <TouchableOpacity style={styles.touchableCancel} onPress={handleClose}>
-        <Text style={styles.textCancel}>Cancel</Text>
-      </TouchableOpacity>
-    );
-  };
-  const renderButtonSubmit = () => {
-    const isActive = notes?.length > 5;
-    const styleView = isActive
-      ? styles.touchableSubmit
-      : styles.touchableSubmitDisabled;
-
-    return (
-      <TouchableOpacity
-        style={styleView}
-        disabled={!isActive}
-        onPress={() => {
-          handleSubmit(notes);
-          handleClose();
-        }}>
-        <Text style={styles.textSubmit}>Submit</Text>
-      </TouchableOpacity>
-    );
-  };
 
   const renderFooter = () => {
     return (
       <View style={styles.footer}>
-        {renderButtonCancel()}
-        {renderButtonSubmit()}
+        <TouchableOpacity style={styles.touchableCancel} onPress={handleClose}>
+          <Text style={styles.textCancel}>Cancel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchableSubmit}
+          onPress={() => {
+            handleSubmit(notes);
+            handleClose();
+          }}>
+          <Text style={styles.textSubmit}>Submit</Text>
+        </TouchableOpacity>
       </View>
     );
   };
