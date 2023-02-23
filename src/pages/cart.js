8e65f2aace324a46d8fd1b1550c363d814ff2f3c
martinwijaya97 <martinwijaya97@gmail.com>
2023-02-23 17:11:05 +0700

    fix

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -846,20 +846,20 @@ const Cart = () => {
     const isPickUp = available && basket?.orderingMode === 'STOREPICKUP';
     const isTakeAway = available && basket?.orderingMode === 'TAKEAWAY';
 
-    // if (isDelivery || isPickUp || isTakeAway) {
-    return (
-      <View style={styles.viewMethod}>
-        <Text style={styles.textMethod}>Order Date</Text>
-        <TouchableOpacity
-          style={styles.touchableMethod}
-          onPress={() => {
-            handleOpenDeliveryDateModal();
-          }}>
-          {renderDateText()}
-        </TouchableOpacity>
-      </View>
-    );
-    // }
+    if (isDelivery || isPickUp || isTakeAway) {
+      return (
+        <View style={styles.viewMethod}>
+          <Text style={styles.textMethod}>Order Date</Text>
+          <TouchableOpacity
+            style={styles.touchableMethod}
+            onPress={() => {
+              handleOpenDeliveryDateModal();
+            }}>
+            {renderDateText()}
+          </TouchableOpacity>
+        </View>
+      );
+    }
   };
 
   const renderAddButton = () => {
