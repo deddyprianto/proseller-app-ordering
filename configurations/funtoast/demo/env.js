5be1ce15358b86491e08906307253beac51cb226
martinwijaya97 <martinwijaya97@gmail.com>
2023-04-18 14:51:34 +0700

    funtoast

---
+++ configurations/funtoast/demo/env.js
@@ -0,0 +1,51 @@
+const ENV = {
+  APP: 'Edgeworks',
+  IS_DEVELOP: true,
+
+  API_HOST: 'https://api-funtoast.proseller-demo.com/', //DEMO
+
+  base_url_payment: 'https://payment.proseller-demo.com/api/', //DEMO COM
+
+  ONESIGNAL_ID: 'ecc78781-3da8-4d14-9df9-f6930d402523',
+
+  REGION: 'ap-southeast-1',
+
+  ENDPOINT: 'https://xxxxxxxxxx.execute-api.ap-southeast-1.amazonaws.com/dev',
+
+  PHONE_NUMBER_CODE: '+65',
+
+  COUNTRY: 'Singapore',
+
+  COMPANY_NAME: 'Funtoast',
+
+  COMPANY_POLICY_URL: 'https://funtoast.proseller.io',
+
+  CURRENCY: 'SGD',
+
+  LOCALES: 'en',
+
+  SENTRY_DSN:
+    'https://57b71e8fbd404c2bb5921afbb00b6c5c@o354451.ingest.sentry.io/2653850',
+
+  PRIVATE_KEY_RSA:
+    'LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQogICAgICAgICAgICAgICAgICBNSUlDWEFJQkFBS0JnUUN0blg5Q3FSWlE0SndWRmxmdzd3TW9qdmxhVGt6clpZQ2d5alQ2L1prdUZDWWhkMytTCiAgICAgICAgICAgICAgICAgIEJUUGo0LzNMSkdwK0lnbnRLNENvVjVpQXFvdm9YQVBkM2Fkb0UrTUdsKzRtM2diK0xjVnpWOHIvbmNqdzIxaGgKICAgICAgICAgICAgICAgICAgRVIxY3JvaTljK3dORmhkYVRlemJTakZmNkdjVnNuNUFQMHNtcnA5SVFTeFNrWmFnbTRzWGtOSi9GUUlEQVFBQgogICAgICAgICAgICAgICAgICBBbjg2V1lqVVRjTThaS3l5UVRpWFMxRnBmQUk5OWZGSk9aUCtKWERsbVZ5YmJYUklXS2YybDFhRkRvcGNDVjNPCiAgICAgICAgICAgICAgICAgIHJ3Y01GMW1WY0dIOHBuMkF4bDZ1dktSMWlCdml2NTVqRE5lTnFGYm9hODhaOTk2aERxdm1iTTVwV0FJbk4rNWwKICAgICAgICAgICAgICAgICAgQTlodEhLRTQrTDIvSWhnaGZWa0ticEpYMlN0dzRpdmZ1ODRVcXF6WGlkamhBa0VBNVg1NldqbHhic25IV2t6VgogICAgICAgICAgICAgICAgICBvZFNxU0JEalBtUzVxZFVRTC9pWnJyWVQwZnJKazIyR1ZIaFdjSk9kTjA0UFRKZ3ZncFUyRXdXOHBITnR2clIwCiAgICAgICAgICAgICAgICAgIFlKcU5ZUUpCQU1HcTFSZVptdzNic29wWVc0NHF6aHZuRG1yc3A4Z21yNUVCaHJzMkcyaHU0RlJpaW1rZ2FJdUYKICAgICAgICAgICAgICAgICAgbzNQdVNrU0tOdDh3ZWowVnVxaEo3QXYxQi9OVGVqVUNRUUNnUjB3V2RXWDJsVEFJcmY3SjdtZ2F1c2lxeTlncgogICAgICAgICAgICAgICAgICBkNlc5aXlkeU1MSDVCZWtBY1E3UE1kaFUrWk5raTd6OXBwQVA2RHdDM3cvWDN0SDN6Ym8xdWRzaEFrRUFpejJXCiAgICAgICAgICAgICAgICAgIDdkdmJsakFpQXNPYU4vSGd0RUVBcGJHMncvbkVlcjRkTDFhc2gvNUh2WVFIdmFPMlpySFRuaE55UkNzWFhvcU4KICAgICAgICAgICAgICAgICAgS0lRSjZ0c1RJbG11dUt5SkRRSkJBTDRnaGVDTm53dlpUT0xtZlJtT01wSCszd2NPWWxSYlpQazlCZFpteHBxbgogICAgICAgICAgICAgICAgICA3MHNsbkcydDBBVWVlR1hNZFl3S3hZSTh2WXVsRWRrR2xsbS9FQURRaHlBPQogICAgICAgICAgICAgICAgICAtLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQ==',
+
+  appUrl: 'https://ordering-funtoast.proseller.io/',
+
+  appID: 'com.funtoast.app',
+
+  retailerInfo: 'Ace Dynamic',
+
+  netsclick_terminal_id: '70235101',
+
+  netsclick_retailer_id: '11170235100',
+
+  netsclick_merchantHost:
+    'https://0pwnewl084.execute-api.ap-southeast-1.amazonaws.com/prod',
+
+  netsclick_api_key: 'f2e7b0fe-4c2e-4f82-9988-e8f3dffe49db',
+
+  netsclick_secret_key: '4bb2d25f-0f0e-4049-995e-e54a05503c65',
+};
+
+export default ENV;
