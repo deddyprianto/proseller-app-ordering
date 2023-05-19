a02cb460aafb67108b93d3652a61cbfd748227eb
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-05-19 10:39:52 +0700

    improve ios build

---
+++ ios/acemart/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  acemart
+//
+//  Created by Agita Firstawan on 19/05/23.
+//  Copyright © 2023 Automaxima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
