eafd9b509026344b85858ef01131b9382065a858
martinwijaya97 <martinwijaya97@gmail.com>
2023-05-23 21:08:12 +0700

    PROS-1022--feat/validation-payment-page-when-ordering-mode-offline

--- src/pages/NewPageIndex.js
+++ src/pages/NewPageIndex.js
@@ -166,7 +166,7 @@ const NewPageIndex = () => {
     Profile: Profile,
   };
 
-  const screens = true ? dataRetailScreens : dataFnBScreens;
+  const screens = false ? dataRetailScreens : dataFnBScreens;
 
   const handleGetNotification = () => {
     OneSignal.setNotificationWillShowInForegroundHandler(
@@ -289,10 +289,9 @@ const NewPageIndex = () => {
     return (
       <SafeAreaView style={styles.root}>
         {handleGetNotification()}
-        {renderHistoryNotificationModal()}
-        <HistoryNotificationModal />
 
         <Tabs />
+        {renderHistoryNotificationModal()}
       </SafeAreaView>
     );
   } else {
