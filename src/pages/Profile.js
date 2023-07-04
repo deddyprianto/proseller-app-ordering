a21cd0b4bc9de93a8c5b7e1d4618322d22bb34f8
martinwijaya97 <martinwijaya97@gmail.com>
2023-07-04 14:28:06 +0700

    PROS-2543--feat/hide-and-show-referral-in-profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -159,6 +159,8 @@ const useStyles = () => {
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
+      borderBottomWidth: 1,
+      borderBottomColor: theme.colors.greyScale3,
     },
     viewPointHeader: {
       alignItems: 'flex-end',
@@ -654,19 +656,12 @@ const Profile = props => {
         {renderMembershipQRCode()}
         {renderDivider()}
         {renderMyDeliveryAddress()}
-        {renderDivider()}
         {renderEditProfile()}
-        {renderDivider()}
         {renderReferral()}
-        {renderDivider()}
         {renderNotifications()}
-        {renderDivider()}
         {renderTermsAndConditions()}
-        {renderDivider()}
         {renderFAQ()}
-        {renderDivider()}
         {renderDeleteAccount()}
-        {renderDivider()}
         {renderLogout()}
       </View>
     );
