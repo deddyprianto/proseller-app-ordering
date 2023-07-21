b174931bf94920aec885e7cab9f33b38b65867dd
martinwijaya97 <martinwijaya97@gmail.com>
2023-07-21 10:23:12 +0700

    PROS-2599--feat/qr-code-payment-page-fomopay

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -13,7 +13,7 @@ const Theme = () => {
 
   const colors = {
     //old version
-    primary: '#CF3030',
+    primary: '#00BFCA',
     secondary: '#F9F9F9',
     thirdColor: '#FFEBEB',
     forthColor: '#F9F9F9',
@@ -43,7 +43,7 @@ const Theme = () => {
     textWarningColor: colorSettings?.textWarningColor || 'black',
 
     snackbarSuccess: '#5CD423',
-    snackbarFailed: '#CF3030',
+    snackbarFailed: '#00BFCA',
 
     //new version
 
@@ -53,20 +53,19 @@ const Theme = () => {
 
     badge: '#CE1111',
 
-    accent: '#FFEBEB',
+    accent: '#CCF2F5',
 
     background: 'white',
 
     textPrimary: '#000000',
     textSecondary: '#FFFFFF',
     textTertiary: '#B7B7B7',
-    textQuaternary: '#CF3030',
-    textError: '#CE1111',
+    textQuaternary: '#00BFCA',
 
     semanticError: '#CE1111',
     semanticSuccess: '#5CD523',
 
-    brandPrimary: '#CF3030',
+    brandPrimary: '#00BFCA',
     brandSecondary: '#FFFFFF',
     brandTertiary: '#0E8A94',
 
@@ -76,13 +75,12 @@ const Theme = () => {
     greyScale4: '#F9F9F9',
     greyScale5: '#888787',
 
-    buttonActive: '#CF3030',
+    buttonActive: '#00BFCA',
     buttonStandBy: '#FFFFFF',
     buttonDisabled: '#B7B7B7',
 
     backgroundTransparent1: '#00000033',
     backgroundTransparent2: '#B7B7B7CC',
-    backgroundTransparent3: '#00000099',
     inactiveDot: '#D6D6D6',
     errorColor: '#CE1111',
   };
@@ -95,7 +93,6 @@ const Theme = () => {
     16: 16,
     20: 20,
     24: 24,
-    36: 36,
   };
 
   const fontFamily = {
