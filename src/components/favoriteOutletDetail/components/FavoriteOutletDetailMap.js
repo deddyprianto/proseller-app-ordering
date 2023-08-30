a084989142d6a4ca28565446692938e24397ca74
martinwijaya97 <martinwijaya97@gmail.com>
2023-08-30 14:57:21 +0700

    PROP-3023--fix/map-in-outlet-detail

--- src/components/favoriteOutletDetail/components/FavoriteOutletDetailMap.js
+++ src/components/favoriteOutletDetail/components/FavoriteOutletDetailMap.js
@@ -2,19 +2,9 @@ import React from 'react';
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
 
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-
-const FavoriteOutletDetailMap = ({lat, long, title, description, style}) => {
-  const latitude = 1.29027;
-  const longitude = 103.851959;
+const FavoriteOutletDetailMap = ({lat, long, style}) => {
+  const latitude = Number(lat);
+  const longitude = Number(long);
   const latitudeDelta = 0.001;
   const longitudeDelta = 0.001;
 
