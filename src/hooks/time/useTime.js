d8052f78cc3e560dffe0bf7677300338cb681ceb
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-10-02 12:02:17 +0700

    fix schedule cose open outlet

--- src/hooks/time/useTime.js
+++ src/hooks/time/useTime.js
@@ -22,8 +22,41 @@ const useTime = () => {
     return 'In 0 day';
   };
 
+  const handleOpenStore = (item, isOrderAvailable) => {
+    let operationalHour = [];
+    if (item?.operationalHours && Array.isArray(item?.operationalHours)) {
+      operationalHour = item?.operationalHours;
+    }
+    if (isOrderAvailable) {
+      if (item?.openAllDays) {
+        return true;
+      }
+      const day = moment().day();
+      const getOperationalHour = operationalHour?.find(
+        operational => operational?.day == day,
+      );
+
+      if (getOperationalHour) {
+        const closeTime =
+          moment().format('YYYY-MM-DD ') + getOperationalHour?.close;
+        const openTime =
+          moment().format('YYYY-MM-DD ') + getOperationalHour?.open;
+        const unixTimeNow = moment().unix();
+        const unixTimeClose = moment(closeTime).unix();
+        const unixTimeOpen = moment(openTime).unix();
+        if (unixTimeNow >= unixTimeOpen && unixTimeNow <= unixTimeClose) {
+          return true;
+        }
+        return false;
+      }
+      return false;
+    }
+    return false;
+  };
+
   return {
     convertTime,
+    handleOpenStore,
   };
 };
 
