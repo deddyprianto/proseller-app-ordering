e316883251e3edc17a81cb7f7e865dda3fd406f3
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-10-10 15:49:20 +0700

    adjusted config for ios

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -43,10 +43,11 @@
 // export default awsConfig;
 
 import Config from 'react-native-config';
-import ENV from '../../configurations/acemart/demo/env';
+import ENV from '../../configurations/fareastflora/dev/env';
 // import ENV from '../../env';
 // import ENV from '../../env-demo';
 import Base64 from 'Base64';
+import {Platform} from 'react-native';
 
 let Data = {};
 // if we build to android, then read .env file
@@ -61,7 +62,8 @@ const awsConfig = {
   // AWS CONFIG
   region: Data.REGION,
   endpoint: Data.ENDPOINT,
-  onesignalID: Data.ONESIGNAL_ID,
+  onesignalID:
+    Platform.OS === 'android' ? Data.ONESIGNAL_ID : Data.ONE_SIGNAL_IOS_ID,
 
   // BASE URL CRM
   base_url: `${Data.API_HOST}crm/api`,
@@ -116,5 +118,4 @@ const awsConfig = {
 
   PASSWORD_ONE_MAP: Config.password_map,
 };
-
 export default awsConfig;
