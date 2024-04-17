11af499e522411d6202978aec1f9851e40ae21ca
Muhammad Suardi <suardi@edgeworks.com.sg>
2024-04-17 15:59:40 +0800

    feat(ptt211): init cno

---
+++ configurations/cno/demo/env.js
@@ -0,0 +1,53 @@
+const ENV = {
+  APP: 'CNO',
+  IS_DEVELOP: true,
+
+  API_HOST: 'https://api-cointernational.proseller-demo.com/', //DEMO
+
+  base_url_payment: 'https://payment.proseller-demo.com/api/', //DEMO COM
+
+  ONESIGNAL_ID: 'd0a36bd6-a0d8-4994-9045-bdb55b1dcce2',
+
+  REGION: 'ap-southeast-1',
+
+  ENDPOINT: 'https://xxxxxxxxxx.execute-api.ap-southeast-1.amazonaws.com/dev',
+
+  PHONE_NUMBER_CODE: '+65',
+
+  COUNTRY: 'Singapore',
+
+  COMPANY_NAME: 'Co International',
+
+  COMPANY_TYPE: 'Retail',
+
+  COMPANY_POLICY_URL: 'https://cointernational.proseller.io',
+
+  CURRENCY: 'SGD',
+
+  LOCALES: 'en-US',
+
+  SENTRY_DSN: 'https://9ed8cf3be2e24a3e884a0b15f5f054d0@sentry.io/2653559',
+
+  PRIVATE_KEY_RSA:
+    'LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQogICAgICAgICAgICAgICAgICBNSUlDWEFJQkFBS0JnUUN0blg5Q3FSWlE0SndWRmxmdzd3TW9qdmxhVGt6clpZQ2d5alQ2L1prdUZDWWhkMytTCiAgICAgICAgICAgICAgICAgIEJUUGo0LzNMSkdwK0lnbnRLNENvVjVpQXFvdm9YQVBkM2Fkb0UrTUdsKzRtM2diK0xjVnpWOHIvbmNqdzIxaGgKICAgICAgICAgICAgICAgICAgRVIxY3JvaTljK3dORmhkYVRlemJTakZmNkdjVnNuNUFQMHNtcnA5SVFTeFNrWmFnbTRzWGtOSi9GUUlEQVFBQgogICAgICAgICAgICAgICAgICBBbjg2V1lqVVRjTThaS3l5UVRpWFMxRnBmQUk5OWZGSk9aUCtKWERsbVZ5YmJYUklXS2YybDFhRkRvcGNDVjNPCiAgICAgICAgICAgICAgICAgIHJ3Y01GMW1WY0dIOHBuMkF4bDZ1dktSMWlCdml2NTVqRE5lTnFGYm9hODhaOTk2aERxdm1iTTVwV0FJbk4rNWwKICAgICAgICAgICAgICAgICAgQTlodEhLRTQrTDIvSWhnaGZWa0ticEpYMlN0dzRpdmZ1ODRVcXF6WGlkamhBa0VBNVg1NldqbHhic25IV2t6VgogICAgICAgICAgICAgICAgICBvZFNxU0JEalBtUzVxZFVRTC9pWnJyWVQwZnJKazIyR1ZIaFdjSk9kTjA0UFRKZ3ZncFUyRXdXOHBITnR2clIwCiAgICAgICAgICAgICAgICAgIFlKcU5ZUUpCQU1HcTFSZVptdzNic29wWVc0NHF6aHZuRG1yc3A4Z21yNUVCaHJzMkcyaHU0RlJpaW1rZ2FJdUYKICAgICAgICAgICAgICAgICAgbzNQdVNrU0tOdDh3ZWowVnVxaEo3QXYxQi9OVGVqVUNRUUNnUjB3V2RXWDJsVEFJcmY3SjdtZ2F1c2lxeTlncgogICAgICAgICAgICAgICAgICBkNlc5aXlkeU1MSDVCZWtBY1E3UE1kaFUrWk5raTd6OXBwQVA2RHdDM3cvWDN0SDN6Ym8xdWRzaEFrRUFpejJXCiAgICAgICAgICAgICAgICAgIDdkdmJsakFpQXNPYU4vSGd0RUVBcGJHMncvbkVlcjRkTDFhc2gvNUh2WVFIdmFPMlpySFRuaE55UkNzWFhvcU4KICAgICAgICAgICAgICAgICAgS0lRSjZ0c1RJbG11dUt5SkRRSkJBTDRnaGVDTm53dlpUT0xtZlJtT01wSCszd2NPWWxSYlpQazlCZFpteHBxbgogICAgICAgICAgICAgICAgICA3MHNsbkcydDBBVWVlR1hNZFl3S3hZSTh2WXVsRWRrR2xsbS9FQURRaHlBPQogICAgICAgICAgICAgICAgICAtLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQ==',
+
+  appUrl: 'maison66://app',
+
+  appID: 'com.edgeworks.cno-demo',
+
+  retailerInfo: '',
+
+  netsclick_terminal_id: '',
+
+  netsclick_retailer_id: '',
+
+  netsclick_merchantHost: '',
+
+  netsclick_api_key: '',
+
+  netsclick_secret_key: '',
+
+  environment: 'demo',
+};
+
+export default ENV;
---
+++ native/cno/android/app/src/main/java/com/funtoast/MainApplication.java
@@ -0,0 +1,127 @@
+package com.funtoast;
+
+// import com.funtoast.generated.BasePackageList;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+// import io.invertase.firebase.crashlytics.ReactNativeFirebaseCrashlyticsPackage;
+// import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
+import com.horcrux.svg.SvgPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+// react native unimodules
+import java.util.Arrays;
+
+// import org.unimodules.adapters.react.ModuleRegistryAdapter;
+// import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+// import org.unimodules.core.interfaces.SingletonModule;
+
+// Facebook login
+// import com.facebook.CallbackManager;
+// import com.facebook.FacebookSdk;
+// import com.facebook.appevents.AppEventsLogger;
+
+
+// Multidex
+import androidx.multidex.MultiDexApplication;
+
+// import com.microsoft.codepush.react.CodePush;
+
+
+public class MainApplication extends MultiDexApplication implements ReactApplication {
+  // private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  // protected static CallbackManager getCallbackManager() {
+  //   return mCallbackManager;
+  // }
+
+  // private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    //@Override
+    //protected String getJSBundleFile() {
+    //    return CodePush.getJSBundleFile();
+    //}
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
+      // packages.add(new MyReactNativePackage());
+
+      // Add unimodules
+        // List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+        //   new ModuleRegistryAdapter(mModuleRegistryProvider)
+        // );
+        // packages.addAll(unimodules);
+
+      return packages;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    // FacebookSdk.sdkInitialize(getApplicationContext());
+    // AppEventsLogger.activateApp(this);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         * We use reflection here to pick up the class that initializes Flipper, since
+         * Flipper library is not available in release mode
+         */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
